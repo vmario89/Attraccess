74672ce25d1787735067e814ff642705760d677a
Jan Jaap <mail@janjaap.de>
2025-05-06 16:02:55 +0200

    feat: pwa install prompt

---
+++ apps/frontend/src/components/pwa-install.tsx
@@ -0,0 +1,28 @@
+import PWAInstallFromLib from '@khmyznikov/pwa-install/react-legacy';
+import de from './pwa-install.de.json';
+import en from './pwa-install.en.json';
+import { useTranslations } from '@attraccess/plugins-frontend-ui';
+
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    promptEvent: any;
+  }
+}
+
+export function PWAInstall() {
+  const { t } = useTranslations('pwaInstall', {
+    en,
+    de,
+  });
+
+  console.log(de, en);
+
+  return (
+    <PWAInstallFromLib
+      name="Attraccess"
+      description={t('description')}
+      icon={'/android-chrome-512x512.png'}
+    ></PWAInstallFromLib>
+  );
+}
