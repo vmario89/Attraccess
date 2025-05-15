7cb2e7f6609aa391d7233ca8ecf935aac3029161
Jan Jaap <mail@janjaap.de>
2025-05-15 21:51:06 +0200

    WIP: use attraccess host

--- libs/plugin-fabreader/firmware/include/configuration.hpp
+++ libs/plugin-fabreader/firmware/include/configuration.hpp
@@ -5,7 +5,7 @@
 #define WIFI_SSID "Jappy-ToGo"
 #define WIFI_PASSWORD "tZGAAWiR6finBH"
 
-#define SERVER_HOSTNAME "192.168.8.230"
-#define SERVER_PORT 3000
+#define SERVER_HOSTNAME "attraccess.cloud.attraktor.org"
+#define SERVER_PORT 80
 
 #define I2C_FREQ 100000
