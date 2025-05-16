86b0342ec026936ec36307ff0755fca2385bf218
Jan Jaap <mail@janjaap.de>
2025-05-16 16:39:31 +0200

    feat(32-make-session-notes-less-frightening): updated the usage start/end buttons to immediately start/stop unless explicitlity selecting with notes option

--- apps/frontend/src/app/resources/usage/translations/resourceUsageSession.de.ts
+++ apps/frontend/src/app/resources/usage/translations/resourceUsageSession.de.ts
@@ -5,16 +5,13 @@ export default {
   startSession: 'Ressource nutzen',
   endSession: 'Sitzung beenden',
   sessionStarted: 'Sitzung gestartet',
-  sessionStartedDescription:
-    'Sie haben die Nutzung dieser Ressource erfolgreich gestartet.',
+  sessionStartedDescription: 'Sie haben die Nutzung dieser Ressource erfolgreich gestartet.',
   sessionStartError: 'Fehler beim Starten der Sitzung',
-  sessionStartErrorDescription:
-    'Beim Starten der Sitzung ist ein Fehler aufgetreten. Bitte versuchen Sie es erneut.',
+  sessionStartErrorDescription: 'Beim Starten der Sitzung ist ein Fehler aufgetreten. Bitte versuchen Sie es erneut.',
   sessionEnded: 'Sitzung beendet',
   sessionEndedDescription: 'Ihre Nutzungssitzung wurde erfolgreich beendet.',
   sessionEndError: 'Fehler beim Beenden der Sitzung',
-  sessionEndErrorDescription:
-    'Beim Beenden der Sitzung ist ein Fehler aufgetreten. Bitte versuchen Sie es erneut.',
+  sessionEndErrorDescription: 'Beim Beenden der Sitzung ist ein Fehler aufgetreten. Bitte versuchen Sie es erneut.',
   elapsedTime: 'Verstrichene Zeit',
   needsIntroduction:
     'Sie benötigen eine Einführung, bevor Sie diese Ressource nutzen können. Bitte kontaktieren Sie einen der unten aufgeführten Einführer.',
@@ -22,4 +19,18 @@ export default {
   noIntroducersAvailable:
     'Für diese Ressource sind derzeit keine Einführer verfügbar. Bitte kontaktieren Sie einen Administrator.',
   resourceInUseBy: 'Ressource wird derzeit verwendet von:',
+  alternativeEndSessionOptionsMenu: {
+    label: 'Alternative Sitzungsende',
+    endWithNotes: {
+      label: 'Mit Notizen beenden',
+      description: 'Beenden Sie die Sitzung und geben Sie Notizen ab.',
+    },
+  },
+  alternativeStartSessionOptionsMenu: {
+    label: 'Alternative Sitzungstart',
+    startWithNotes: {
+      label: 'Mit Notizen starten',
+      description: 'Starten Sie die Sitzung und geben Sie Notizen ab.',
+    },
+  },
 };
